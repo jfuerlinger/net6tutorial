25335db167c38d95069d24003203b0a20189ca9d
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2021-07-18 11:18:14 +0200

    Remove code smell

--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging.Console;
 
 namespace catalog
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
